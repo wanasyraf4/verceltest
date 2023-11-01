36c55ef36d2b9f31b9c91f5bdde62eb24f4ffdab
Wan Asyraf <107595740+wanasyraf4@users.noreply.github.com>
2023-11-01 13:11:43 +0800

    Create index.py

---
+++ src/index.py
@@ -0,0 +1,45 @@
+import os
+from flask import Flask
+import numpy as np
+
+app = Flask(__name__)
+
+print(os.listdir(os.getcwd()))
+
+@app.route('/')
+def home():
+    print(os.listdir(os.getcwd()))
+    return 'Home Page Route'
+
+
+@app.route('/about')
+def about():
+    return 'About Page Route'
+
+
+@app.route('/portfolio')
+def portfolio():
+    a = np.random.choice([1, 2, 3, 4, 5, 6])
+    return f'Portfolio {a} Page Route'
+
+
+@app.route('/contact')
+def contact():
+    return 'Contact Page Route'
+
+
+@app.route('/api')
+def api():
+    with open(
+            os.path.join(os.getcwd(), 'data', 'data.json'),
+            mode='r'
+    ) as my_file:
+        text = my_file.read()
+        return text
+
+
+if __name__ == '__main__':
+    #port = int(os.environ.get('PORT', 5000))
+    print(os.getcwd())
+    #app.run(host='0.0.0.0', port=port)
+
